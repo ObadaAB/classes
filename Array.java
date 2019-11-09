5fa70ec3f2d53bb07849c79e54034e4c591f269b
Obada Alabbadi <abadiobada@gmail.com>
2019-11-09 19:16:39 +0200

    Initial commit

---
+++ Array.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Array
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the number of elements :");
+        int n = in.nextInt();
+
+        int [] a = new int[n];
+
+        System.out.println("Enter " + n + " integers :");
+
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = in.nextInt();
+        }
+
+        int mx = a[0], mn = a[0];
+        double avg = 0;
+        for (int i = 0; i < n; i++)
+        {
+            mx = Math.max(mn, a[i]);
+            mn = Math.min(mn, a[i]);
+            avg = avg + ((double)a[i]) / n;
+        }
+
+        System.out.println("Maximum : " + mx);
+        System.out.println("Minimum : " + mn);
+        System.out.println("Average : " + avg);
+    }
+}
