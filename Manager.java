5d6ce6dcd7c65ce2e65e7d8f7dbb68a0a79d4426
ObadaAB <42376288+ObadaAB@users.noreply.github.com>
2020-01-16 18:58:22 +0200

    Add files via upload

--- Manager.java
+++ Manager.java
@@ -1,25 +1,33 @@
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.net.Inet4Address;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Manager
 {
     private static ServerSocket serverSocket;
     private static int port = 8080;
 
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException, ClassNotFoundException
     {
-        System.out.println(Inet4Address.getLocalHost().getHostAddress());
         serverSocket = new ServerSocket(port);
-        Socket socket = serverSocket.accept();
-        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-        String s = dataInputStream.readUTF();
-        System.out.println(s);
 
-        dataInputStream.close();
-        socket.close();
+        int n = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < n; i++)
+        {
+            Socket socket = serverSocket.accept();
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            TreeMap<String, Integer> treeMap = (TreeMap<String, Integer>) objectInputStream.readObject();
+            for (Map.Entry<String, Integer> x : treeMap.entrySet())
+            {
+                System.out.println(i + " " + x.getKey() + " " + x.getValue());
+            }
+            objectInputStream.close();
+            socket.close();
+        }
+
         serverSocket.close();
     }
 }
---
+++ Mapper.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.TreeMap;
+
+public class Mapper
+{
+    private static Socket socket;
+    private static int port = 8080;
+
+    public static void main(String[] args) throws IOException
+    {
+        String host = args[0];
+        socket = new Socket(host, port);
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        TreeMap<String, Integer> mp = new TreeMap<>();
+
+        for (int i = 1; i < args.length; i++)
+        {
+            if (mp.containsKey(args[i]))
+            {
+                mp.put(args[i], mp.get(args[i]) + 1);
+            }
+            mp.put(args[i], 1);
+        }
+
+        objectOutputStream.writeObject(mp);
+
+        objectOutputStream.close();
+        socket.close();
+    }
+}
