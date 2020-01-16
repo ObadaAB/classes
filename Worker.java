3acc71b7a6dca5d5e3d4bcdc5f9c91c7e0c23891
ObadaAB <42376288+ObadaAB@users.noreply.github.com>
2020-01-16 13:58:18 +0200

    Add files via upload

--- Worker.java
+++ Worker.java
@@ -2,6 +2,7 @@ import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Worker
 {
@@ -10,7 +11,7 @@ public class Worker
 
     public static void main(String[] args) throws IOException
     {
-        String host = args[0];
+        String host = (new Scanner(System.in)).next();
         socket = new Socket(host, port);
         DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
         dataOutputStream.writeUTF("IT'S WORKING!!!!");
